71ad2a2998890e4a6e6f2e45380549fd43bb239f
Marek Chrappa <chrappamarek@gmail.com>
2022-03-29 14:00:02 +0200

    add apis

--- mtaa_app/backend/server.js
+++ mtaa_app/backend/server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 var express = require('express');
 var app = express();
 var cors = require('cors')
+var crypto = require('crypto');
 
 app.use(cors())
 
@@ -88,101 +89,188 @@ app.get('/places/reviews/:placeid', function (req, res) {
 app.post('/places/create/:placetype/:userid', function (req, res) {
     connectDB();
     console.log('POST received');
-    req.on('data', function(data){
-        let input = JSON.parse(data);
-        res.setHeader('Content-Type', 'application/json');
-        connection.query("INSERT INTO Places (name, shortDescription, longDescription, photo, placeType, location) VALUES \
-        ('"+input.name+"','"+input.shortDescription+"','"+input.longDescription+"','"+input.photo+"','"+req.params.placetype+"','"+input.location+"');", 
-        function (error, results) {
-            if (error) {
-                res.statusCode = 500;
-                console.log(error)
-                res.end(JSON.stringify({
-                    'status': 'error',
-                    'json': input
-                }));
-            }
-            else{
-                res.statusCode = 200;
-                res.end(JSON.stringify({'status': 'ok'}));
-            }
+    if(parseInt(req.params.userid)>0){
+        req.on('data', function(data){
+            let input = JSON.parse(data);
+            res.setHeader('Content-Type', 'application/json');
+            connection.query("INSERT INTO Places (name, shortDescription, longDescription, photo, placeType, location) VALUES \
+            ('"+input.name+"','"+input.shortDescription+"','"+input.longDescription+"','"+input.photo+"','"+req.params.placetype+"','"+input.location+"');", 
+            function (error, results) {
+                if (error) {
+                    res.statusCode = 500;
+                    console.log(error)
+                    res.end(JSON.stringify({
+                        'status': 'error',
+                        'json': input
+                    }));
+                }
+                else{
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({'status': 'ok'}));
+                }
+            });
         });
-    });
+    }
+    else{
+        res.statusCode = 401;
+        res.end(JSON.stringify({'status': 'unauthorized'}));
+    }
 });
 
 //post na vlozenie recenzií
 app.post('/places/reviews/create/:placeid/:userid', function (req, res) {
     connectDB();
     console.log('POST received');
+    if(parseInt(req.params.userid)>0){
+        req.on('data', function(data){
+            let input = JSON.parse(data);
+            res.setHeader('Content-Type', 'application/json');
+            connection.query("INSERT INTO Reviews (placeUID, userUsername, reviewText, revPhoto, rating, userUID) VALUES \
+            ('"+req.params.placeid+"','"+input.userUsername+"','"+input.reviewText+"','"+input.revPhoto+"','"+input.rating+"' ,'"+req.params.userid+"');", 
+            function (error, results) {
+                if (error) {
+                    res.statusCode = 500;
+                    console.log(error)
+                    res.end(JSON.stringify({
+                        'status': 'error',
+                        'json': input
+                    }));
+                }
+                else{
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({'status': 'ok'}));
+                }
+            });
+        });
+    }
+});
+
+//put na upravu miesta
+app.put('/places/edit/:placeid/:userid', function (req, res) {
+    connectDB();
+    console.log('POST received');
     req.on('data', function(data){
         let input = JSON.parse(data);
         res.setHeader('Content-Type', 'application/json');
-        connection.query("INSERT INTO Reviews (placeUID, userUsername, reviewText, revPhoto, rating, userUID) VALUES \
-        ('"+req.params.placeid+"','"+input.userUsername+"','"+input.reviewText+"','"+input.revPhoto+"','"+input.rating+"' ,'"+req.params.userid+"');", 
-        function (error, results) {
+        connection.query("SELECT isAdmin from Users WHERE userUID = "+req.params.userid
+        , function (error, results) {
             if (error) {
                 res.statusCode = 500;
                 console.log(error)
                 res.end(JSON.stringify({
-                    'status': 'error',
-                    'json': input
+                    'status': 'error'
                 }));
             }
+            if(results[0].isAdmin){
+                connection.query("UPDATE Places SET \
+                uniqueID = '"+req.params.placeid+"', \
+                name = '"+input.name+"', \
+                shortDescription = '"+input.shortDescription+"', \
+                longDescription = '"+input.longDescription+"', \
+                photo = '"+input.photo+"', \
+                placeType = '"+input.placeType+"', \
+                location = '"+input.location+"' \
+                WHERE uniqueID = "+req.params.placeid+""
+                , function (error, results) {
+                    if (error) {
+                        res.statusCode = 500;
+                        console.log(error)
+                        res.end(JSON.stringify({
+                            'status': 'error',
+                            'json': input
+                        }));
+                    }
+                    else{
+                        res.statusCode = 200;
+                        res.end(JSON.stringify({'status': 'ok'}));
+                    }
+                });
+            }
             else{
-                res.statusCode = 200;
-                res.end(JSON.stringify({'status': 'ok'}));
+                res.statusCode = 401;
+                res.end(JSON.stringify({'status': 'unauthorized'}));
             }
         });
+        
     });
 });
 
-//delete na vlozenie recenzií
+//delete na vymazanie recenzií
 app.delete('/places/reviews/delete/:reviewid/:userid', function (req, res) {
     connectDB();
     console.log('DELETE received');
-    req.on('data', function(data){
-        let input = JSON.parse(data);
-        res.setHeader('Content-Type', 'application/json');
-        connection.query('DELETE FROM Reviews WHERE reviewID = ' +req.params.reviewid, function (error, results) {
-            if (error) {
-                res.statusCode = 500;
-                console.log(error)
-                res.end(JSON.stringify({
-                    'status': 'error',
-                    'json': input
-                }));
-            }
-            else{
-                res.statusCode = 200;
-                res.end(JSON.stringify({'status': 'ok'}));
-            }
-        });
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    connection.query("SELECT isAdmin from Users WHERE userUID = "+req.params.userid
+    , function (error, results) {
+        if (error) {
+            res.statusCode = 500;
+            console.log(error)
+            res.end(JSON.stringify({
+                'status': 'error'
+            }));
+        }
+        if(results[0].isAdmin){
+            connection.query('DELETE FROM Reviews WHERE reviewID = ' +req.params.reviewid, function (error, results) {
+                if (error) {
+                    res.statusCode = 500;
+                    console.log(error)
+                    res.end(JSON.stringify({
+                        'status': 'error'
+                    }));
+                }
+                else{
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({'status': 'ok'}));
+                }
+            });
+        }
+        else{
+            res.statusCode = 401;
+            res.end(JSON.stringify({'status': 'unauthorized'}));
+        }
     });
 });
 
+//delete na odstranenie miesta
 app.delete('/places/delete/:placeid/:userid', function (req, res) {
     connectDB();
     console.log('DELETE received');
-    req.on('data', function(data){
-        let input = JSON.parse(data);
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        connection.query('DELETE FROM Places WHERE uniqueID = ' +req.params.placeid, function (error, results) {
-            if (error) {
-                res.statusCode = 500;
-                console.log(error)
-                res.end(JSON.stringify({
-                    'status': 'error',
-                    'json': input
-                }));
-            }
-            else{
-                res.statusCode = 200;
-                res.end(JSON.stringify({'status': 'ok'}));
-            }
-        });
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    connection.query("SELECT isAdmin from Users WHERE userUID = "+req.params.userid
+    , function (error, results) {
+        if (error) {
+            res.statusCode = 500;
+            console.log(error)
+            res.end(JSON.stringify({
+                'status': 'error'
+            }));
+        }
+        if(results[0].isAdmin){
+            connection.query('DELETE FROM Places WHERE uniqueID = ' +req.params.placeid, function (error, results) {
+                if (error) {
+                    res.statusCode = 500;
+                    console.log(error)
+                    res.end(JSON.stringify({
+                        'status': 'error'
+                    }));
+                }
+                else{
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({'status': 'ok'}));
+                }
+            });
+        }
+        else{
+            res.statusCode = 401;
+            res.end(JSON.stringify({'status': 'unauthorized'}));
+        }
     });
 });
+
+
+
 
 app.listen(port, function () {
     console.log('Example app listening on port 3000.');
