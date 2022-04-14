0d51550cfe8777f08ff981023cf8c628e93ed229
Marek Chrappa <chrappamarek@gmail.com>
2022-04-14 11:50:00 +0200

    get and put changed

--- frontend_app/src/main/java/com/example/mtaaa/Log_screen.java
+++ frontend_app/src/main/java/com/example/mtaaa/Log_screen.java
@@ -4,13 +4,20 @@ import android.app.DownloadManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ResourceCursorAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -26,6 +33,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,13 +41,19 @@ import java.util.Map;
 
 public class Log_screen extends AppCompatActivity {
 
-    
+    Button button;
+    private static String rJson;
+    public void Log_setrJson(String str){
+        Log_screen.rJson = str;
+    }
+    public String Log_getrJson(){ return Log_screen.rJson;}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        Button button = findViewById(R.id.button_logg);
+        button = findViewById(R.id.button_logg);
         button.setOnClickListener(v -> loggin(JSONSaved.getUrl()+"/users/login"));
     }
 
@@ -50,18 +64,98 @@ public class Log_screen extends AppCompatActivity {
         EditText passField = findViewById(R.id.Password);
         String password = passField.getText().toString();
 
-        JSONObject user = new JSONObject();
-        try {
-            user.put("name", name);
-            user.put("pw",password);
+        JSONObject jsonBody = new JSONObject();
 
-        } catch (JSONException e)
-        {
+        try {
+            jsonBody.put("username", "admin");
+            jsonBody.put("pw", "heslo");
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
+        final String requestBody = jsonBody.toString();
 
-        /*
+        RequestQueue queue = Volley.newRequestQueue(this);
+        //for POST requests, only the following line should be changed to
+
+        StringRequest sr = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log_setrJson(response);
+                        Logg();
+                        Log.e("HttpClient", "success! response: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("HttpClient", "error: " + error.toString());
+                    }
+                })
+        {
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            /*
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("username","admin");
+                params.put("pw","heslo");
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","raw");
+                return params;
+            }
+
+             */
+        };
+        queue.add(sr);
+
+        //Intent intent = new Intent(this, Home_screen.class);
+        //startActivity(intent);
+    }
+
+    void Logg()
+    {
+        button.setText("som tu");
+        try {
+            JSONObject obj = new JSONObject(Log_getrJson());
+            if(obj.has("Status")){
+
+                String Status = obj.getString("Status");
+                button.setText("Status");
+                int Admin = obj.getInt("admin");
+                int UID = obj.getInt("UID");
+
+            }
+        } catch (JSONException e) {
+            Log.e("JSONERROR", "unexpected JSON exception", e);
+        }
+    }
+
+}
+
+
+/*
 
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest str = new StringRequest(Request.Method.POST, "https://e190-95-102-14-246.eu.ngrok.io/users/register",
@@ -120,48 +214,3 @@ public class Log_screen extends AppCompatActivity {
 
 
          */
-
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        //for POST requests, only the following line should be changed to
-
-        StringRequest sr = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("HttpClient", "success! response: " + response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("HttpClient", "error: " + error.toString());
-                        //button.setText(error.toString());
-                    }
-                })
-        {
-            @Override
-            protected Map<String,String> getParams(){
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("username","admin");
-                params.put("pw","heslo");
-                return params;
-            }
-            /*
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("Content-Type","raw");
-                return params;
-            }
-
-             */
-        };
-        queue.add(sr);
-
-        //Intent intent = new Intent(this, Home_screen.class);
-        //startActivity(intent);
-    }
-
-
-}
