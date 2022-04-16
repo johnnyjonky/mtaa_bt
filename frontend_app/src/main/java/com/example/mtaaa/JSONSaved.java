4dd8ab05626f0d17796f7db5dada429c7bd50e37
Marek Chrappa <chrappamarek@gmail.com>
2022-04-17 00:10:01 +0200

    more options in JSONSaved

--- frontend_app/src/main/java/com/example/mtaaa/JSONSaved.java
+++ frontend_app/src/main/java/com/example/mtaaa/JSONSaved.java
@@ -1,12 +1,14 @@
 package com.example.mtaaa;
 
 public class JSONSaved {
+
+    private static String user_name;
     private static int user = 0;
     private static int isadmin = 0;
     private static int placetype;
     private static int placeid;
     private static String placetypeName;
-    private static String url = "https://6159-147-175-190-171.ngrok.io";
+    private static String url = "https://6ff6-95-102-14-246.eu.ngrok.io";
 
     public static void setUser(int user){
         JSONSaved.user = user;
@@ -15,11 +17,14 @@ public class JSONSaved {
         return JSONSaved.user;
     }
 
+    public static void setUser_name(String name){ JSONSaved.user_name = name; }
+    public static String getUser_name(){ return JSONSaved.user_name; }
+
     public static void setIsadmin(int admin){
         JSONSaved.isadmin = admin;
     }
 
-    public int getIsadmin(){
+    public static int getIsadmin(){
         return JSONSaved.isadmin;
     }
 
